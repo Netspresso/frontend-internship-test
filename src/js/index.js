cabfdd6bb5bf99bb16f988e10c40b2a503eb76b5
Dawid Marszalik <proconqpa@gmail.com>
2021-02-18 19:58:14 +0100

    Add simply JS form validation and custom checkbox

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 var btnMain = document.getElementById('show-popup-form');
 var btnExit = document.querySelector('i');
 var popup = document.querySelector('div.popup');
+var email = document.getElementById('email')
+var pass = document.getElementById('password')
 
 function showPopup() {
     popup.classList.toggle('active');
@@ -8,3 +10,26 @@ function showPopup() {
 
 btnMain.addEventListener('click', showPopup);
 btnExit.addEventListener('click', showPopup);
+
+
+// Validation of form inputs
+
+// E-mail validation:
+email.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+      email.setCustomValidity("I am expecting an e-mail address!");
+    } else {
+      email.setCustomValidity("");
+    }
+});
+  
+
+// Password validation:
+pass.addEventListener("input", function (event) {
+    if (pass.validity.tooShort) {
+      pass.setCustomValidity("Password to short!");
+    } else {
+      pass.setCustomValidity("");
+    }
+});
+  
