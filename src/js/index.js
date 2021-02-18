91909280622abe88dc15ea8c22faf0d45bcb8b44
Dawid Marszalik <proconqpa@gmail.com>
2021-02-18 21:01:41 +0100

    Add more advanced validaion and closing popup after 3s. Display Than you at the end

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,46 @@
 var btnMain = document.getElementById('show-popup-form');
 var btnExit = document.querySelector('i');
 var popup = document.querySelector('div.popup');
-var email = document.getElementById('email')
-var pass = document.getElementById('password')
+var email = document.getElementById('email');
+var pass = document.getElementById('password');
+var checkbox = document.querySelector('.form-check>input');
+var btnSubmit = document.getElementById('submit');
 
 function showPopup() {
     popup.classList.toggle('active');
+}
+
+
+function checkValidity(event) {
+    if (!checkbox.checked) {
+        alert('You must agree to the terms first.');
+        return false;
+    }
+    else if (email.validity.typeMismatch || email.value == '') {
+        alert('I am expecting an e-mail address!')
+    }
+    else if (pass.validity.tooShort) {
+        alert('Password too short')
+    }
+    else if (pass.value == '') {
+        alert('You must provide password')
+        }
+    else {
+        return true;
+    }
+}
+
+function submitForm(e) {
+
+    // checkValidity();
+
+    if (checkValidity()) {
+        // console.log(btnMain);
+        btnMain.textContent = "Thank you!"
+        setTimeout(showPopup, 3000);
+    };
+
+    
 }
 
 btnMain.addEventListener('click', showPopup);
@@ -33,3 +68,6 @@ pass.addEventListener("input", function (event) {
     }
 });
   
+
+btnSubmit.addEventListener("click", submitForm)
+
