ffafb4fd0728c8287a427c40a1995c5694f97c5e
Dawid Marszalik <proconqpa@gmail.com>
2021-02-19 11:31:34 +0100

    Add few comments and do some clean up

--- src/js/index.js
+++ src/js/index.js
@@ -7,40 +7,36 @@ var checkbox = document.querySelector('.form-check>input');
 var btnSubmit = document.getElementById('submit');
 
 function showPopup() {
+    // Function that controll displaying popup window by toggling active class
     popup.classList.toggle('active');
 }
 
 
 function checkValidity(event) {
+    // Function that check validity of data inputed in every used input field
     if (!checkbox.checked) {
         alert('You must agree to the terms first.');
         return false;
-    }
-    else if (email.validity.typeMismatch || email.value == '') {
-        alert('I am expecting an e-mail address!')
-    }
-    else if (pass.validity.tooShort) {
-        alert('Password too short')
-    }
-    else if (pass.value == '') {
-        alert('You must provide password')
-        }
-    else {
+    } else if (email.validity.typeMismatch || email.value == '') {
+        alert('I am expecting an e-mail address!');
+        return false;
+    } else if (pass.validity.tooShort) {
+        alert('Password too short');
+        return false;
+    } else if (pass.value == '') {
+        alert('You must provide password');
+        return false;
+    } else {
         return true;
     }
 }
 
 function submitForm(e) {
-
-    // checkValidity();
-
+    // Function that check validity and if it return true -> popup is closing after 3s and displayed text is changed to 'Thank you!'
     if (checkValidity()) {
-        // console.log(btnMain);
-        btnMain.textContent = "Thank you!"
+        btnMain.textContent = "Thank you!";
         setTimeout(showPopup, 3000);
-    };
-
-    
+    }
 }
 
 btnMain.addEventListener('click', showPopup);
@@ -49,7 +45,7 @@ btnExit.addEventListener('click', showPopup);
 
 // Validation of form inputs
 
-// E-mail validation:
+// E-mail browser API validation:
 email.addEventListener("input", function (event) {
     if (email.validity.typeMismatch) {
       email.setCustomValidity("I am expecting an e-mail address!");
@@ -59,7 +55,7 @@ email.addEventListener("input", function (event) {
 });
   
 
-// Password validation:
+// Password browser API validation:
 pass.addEventListener("input", function (event) {
     if (pass.validity.tooShort) {
       pass.setCustomValidity("Password to short!");
